967fcbc3a16e4ec028536cfdc85e5f30d93ff793
Corey Rigney <coreyrigney@gmail.com>
2020-01-04 12:32:50 -0600

    File IO start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const fs = require('fs');
+const csv = require('csv-parser');
+
+let items = [];
+let buffs = [];
+let misc = [];
+
+class item {
+  constructor(name, cost, code, description){
+    this.name = name;
+    this.cost = cost;
+    this.code = code;
+    this.description = description;
+  }
+}
+
+function readItems(path, collection){
+  let stream = fs.createReadStream(path);
+  stream.pipe(csv());
+  stream.on('data', (row) => {
+    console.log(row);
+  });
+  stream.on('end', () => {
+    console.log('CSV file successfully processed');
+  });
+}
 
 function App() {
+  readItems("items/items.csv", items);
+  readItems("items/buffs.csv", buffs);
+  readItems("items/misc.csv", misc);
   return (
     <div className="App">
       <header className="App-header">
